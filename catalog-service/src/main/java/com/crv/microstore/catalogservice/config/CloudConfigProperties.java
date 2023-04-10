935a94049921530b201db07586b898a29c387818
CRV <robert.crv@icloud.com>
2023-04-10 21:28:49 +0300

    integration of Spring Cloud config + adding FE for storefront and smartadmin

---
+++ catalog-service/src/main/java/com/crv/microstore/catalogservice/config/CloudConfigProperties.java
@@ -0,0 +1,12 @@
+package com.crv.microstore.catalogservice.config;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RefreshScope
+public class CloudConfigProperties {
+
+//    @Value("${}")
+//    public static String URL;
+}
